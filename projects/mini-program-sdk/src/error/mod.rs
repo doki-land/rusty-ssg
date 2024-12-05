0adeded33cf7b6e36a53c801f530d82c4b613985
Aster <galaster@foxmail.com>
2024-12-05 14:55:38 +0800

    Use a unified error handling approach

--- projects/mini-program-sdk/src/error/mod.rs
+++ projects/mini-program-sdk/src/error/mod.rs
@@ -1,4 +1,7 @@
-use std::borrow::Cow;
+use std::{
+    borrow::Cow,
+    fmt::{Display, Formatter},
+};
 
 pub struct WechatError {
     pub code: i32,
@@ -17,5 +20,11 @@ impl WechatError {
 impl From<reqwest::Error> for WechatError {
     fn from(error: reqwest::Error) -> Self {
         WechatError { code: -200, message: Cow::Owned(error.to_string()) }
+    }
+}
+
+impl Display for WechatError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[WechatError={}] {}", self.code, self.message)
     }
 }
